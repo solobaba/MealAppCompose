6b0b76ee681bac64eccb03f728555f5bb57b2958
Solomon <solomon.oduniyi@gmail.com>
2024-02-20 17:15:48 +0100

    Implementing travel card

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3:1.1.2")
 
     //Lifecycle
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
